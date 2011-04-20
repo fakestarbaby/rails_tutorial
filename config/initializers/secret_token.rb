df47bfe1731c7a3e05513778869b02aaebf04ce4
fakestarbaby <fakestarbaby@gmail.com>
2011-04-20 23:20:20 +0900

    初回コミット。

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsTutorial::Application.config.secret_token = '877daaff551eefd226620f6ea43425e37fa93bbc31fcbef97f8b321864eaa370445a083ab1c3ccb05609624499a2f877e3ea2a85405a7d79f4ef6fac22463aaa'
