1baac62dfac707616c858831689307958d9a41a9
fakestarbaby <fakestarbaby@gmail.com>
2011-04-21 21:35:16 +0900

    定数管理プラグインを追加。

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'http://rubygems.org'
 gem 'rails', '3.0.7'
 
 gem 'haml-rails'
+gem 'settingslogic'
 gem 'sqlite3-ruby', :require => 'sqlite3'
 
 group :development do
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def title
+    base_title = Settings.base_title
+    unless @title
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+
 end
